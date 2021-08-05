d0fd2dd07f8cc90d268bc561f234988fc64dfe50
sksatu1 <satueee18@gmail.com>
2021-08-05 10:52:20 +0600

    first commit

---
+++ summary.js
@@ -0,0 +1,18 @@
+// Module 19-5 :
+// 1.core concepts
+// 2. variable (let , const) javascript var, let , const
+// 3. array,loop(while,for),conditionals,function
+
+// 4. Function 
+//     *leap year
+//     * odd even
+//     * factorial (while,for)
+
+
+
+// New home work
+// 1 Celsius to fahrenheit 
+// 2 Fahrenheit to celsius
+// 3 Grade calculate 
+// 4 Simple interest
+
